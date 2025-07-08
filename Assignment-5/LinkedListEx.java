f2d3dcb4eb2e545c2b54071ed339dd3f9ff02846
adityaaaslin <aditya.aaslin@gmail.com>
2025-07-08 15:09:29 +0530

    Assignment-5

---
+++ Assignment-5/LinkedListEx.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class LinkedListEx {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter no of elements in the list");
+        int n=sc.nextInt();
+        LinkedList<Integer> ll=new LinkedList<>();
+        for(int i=0;i<n;i++){
+            ll.add(sc.nextInt());
+    }
+    ll.addFirst(5);
+    ll.addLast(60);
+    System.out.println(ll);
+    ll.remove(2);
+    System.out.println("After removing 2nd index element from the linked list..");
+    System.out.println(ll);
+}
+}
